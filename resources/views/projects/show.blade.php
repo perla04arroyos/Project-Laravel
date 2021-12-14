6f14f8d9e6583c792fae81f4051f8739e6cee4a2
Perla Arroyos <perla.pmar04@gmail.com>
2021-12-13 18:51:17 -0700

    Ad image to show project view

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -6,12 +6,12 @@
 
     <div class="container mt-2">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-8 bg-white p-4 shadow rounded">
 
                 @if($project->image) 
-                    <img src="/storage/{{ $project->image}}" alt="{{ $project->title }}">
+                    <img class="mx-auto d-block mb-4 rounded" src="/storage/{{ $project->image}}" alt="{{ $project->title }}">
                 @endif
-                
+
                 <h1>{{ $project->title }}</h1>
                 <p>{{ $project->description }}</p>
 
